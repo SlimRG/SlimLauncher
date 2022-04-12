9bc586de0203c4843ee7fbe6953fc23f1742054d
Alrott SlimRG <slimrg@ya.ru>
2022-04-13 01:06:19 +0300

    Create widget app

---
+++ qLauncher/src/starter.cpp
@@ -0,0 +1,15 @@
+#include "starter.h"
+#include "ui_starter.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
